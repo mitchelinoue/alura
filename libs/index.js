fbbd7deb8a7501495e31450906962465fad2561b
Mitchel Inoue <107430805+mitchelinoue@users.noreply.github.com>
2022-07-27 11:07:05 -0300

    teste

---
+++ libs/index.js
@@ -0,0 +1,29 @@
+//const chalk = require('chalk');
+import chalk from 'chalk';
+import fs from 'fs';
+
+console.log(chalk.blue('Vamos começar!'));
+
+const paragrafo = 'Texto retornado por uma função';
+
+function texto(string) {
+    return string;
+}
+
+console.log(texto(paragrafo));
+
+//encadear métodos para colorir texto, cor de fundo e texto em negrito
+console.log(chalk.blue.bgWhite.bold('Alura'));
+
+//receber múltiplos argumentos
+console.log(chalk.blue('curso', 'de', 'NodeJS'));
+
+//métodos aninhados
+console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
+
+// uso de template strings e placeholders
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
